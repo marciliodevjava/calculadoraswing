406435a7568dea9e6d532001bc88dd539b56b9d8
m-arc <m-arc@192.168.1.8>
2022-05-09 17:30:43 -0300

    Class Calculadora

--- src/br/com/cod3r/calc/visao/Calculadora.java
+++ src/br/com/cod3r/calc/visao/Calculadora.java
@@ -3,14 +3,17 @@ package br.com.cod3r.calc.visao;
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
-public class Calculadora extends JFrame{
+public class Calculadora extends JFrame {
 
 	public Calculadora() {
-		setVisible(true);
+		setSize(232, 322);// Tamanho da Calculadora
+		setDefaultCloseOperation(EXIT_ON_CLOSE);// Fechar quando aperta no "x"
+		setLocationRelativeTo(null);// Centralizar a interface grafica
+		setVisible(true);// Deixando a ablicação visivel
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		new Calculadora();
 	}
 }
