a84ba8e02ec2c36bfe38a53143766ac76bf98a6f
m-arc <m-arc@192.168.1.8>
2022-05-09 17:25:23 -0300

    Calculadora com Java Swing

---
+++ src/br/com/cod3r/calc/visao/Calculadora.java
@@ -0,0 +1,16 @@
+package br.com.cod3r.calc.visao;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame{
+
+	public Calculadora() {
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		new Calculadora();
+	}
+}
