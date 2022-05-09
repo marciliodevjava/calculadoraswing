a12bfcbe67a1fd056b88bbb65d5b85e35a514945
m-arc <m-arc@192.168.1.8>
2022-05-09 17:41:25 -0300

    Class Calculadora

--- src/br/com/cod3r/calc/visao/Calculadora.java
+++ src/br/com/cod3r/calc/visao/Calculadora.java
@@ -1,15 +1,29 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class Calculadora extends JFrame {
 
 	public Calculadora() {
+		organizarLayout();
 		setSize(232, 322);// Tamanho da Calculadora
 		setDefaultCloseOperation(EXIT_ON_CLOSE);// Fechar quando aperta no "x"
 		setLocationRelativeTo(null);// Centralizar a interface grafica
 		setVisible(true);// Deixando a ablicação visivel
+	}
+
+	private void organizarLayout() {
+		setLayout(new BorderLayout());
+		
+		Display display = new Display();
+		display.setPreferredSize(new Dimension(233,60));
+		add(display, BorderLayout.NORTH);
+		Teclado teclado = new Teclado();
+		add(teclado, BorderLayout.CENTER);
 	}
 
 	public static void main(String[] args) {
