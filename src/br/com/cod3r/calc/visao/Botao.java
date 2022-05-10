2b50de57f413bf818d77dd8e79ca29efcda4c981
m-arc <m-arc@192.168.1.8>
2022-05-09 22:12:39 -0300

    Class Botao

--- src/br/com/cod3r/calc/visao/Botao.java
+++ src/br/com/cod3r/calc/visao/Botao.java
@@ -1,8 +1,20 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton{
 
+	public Botao(String texto, Color cor) {
+		setFont(new Font("courier", Font.PLAIN, 25));
+		setText(texto);
+		setOpaque(true);
+		setBackground(cor);
+		setForeground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
 }
