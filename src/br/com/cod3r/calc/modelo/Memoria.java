d2ea1343d7112e00af8775c107c4549d2649bc89
m-arc <m-arc@192.168.1.8>
2022-05-09 23:09:20 -0300

    Class Memoria

--- src/br/com/cod3r/calc/modelo/Memoria.java
+++ src/br/com/cod3r/calc/modelo/Memoria.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Memoria {
 
+	private enum TipoComando {
+		ZERA, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA;
+	};
+
 	private static final Memoria instancia = new Memoria();
 	private final List<MemoriaObservador> observadores = new ArrayList<>();
 	private String textoAtual = "";
@@ -25,12 +29,44 @@ public class Memoria {
 		return textoAtual.isEmpty() ? "0" : textoAtual;
 	}
 
-	public void processarComando(String valor) {
-		if ("AC".equals(valor)) {
+	public void processarComando(String texto) {
+		TipoComando tipoComando = detectarTipoComando(texto);
+		System.out.println(tipoComando);
+
+		if ("AC".equals(texto)) {
 			textoAtual = "";
 		} else {
-			textoAtual += valor;
+			textoAtual += texto;
 		}
+
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+	}
+
+	private TipoComando detectarTipoComando(String texto) {
+		if (textoAtual.isEmpty() && texto == "0") {
+			return null;
+		}
+		try {
+			Integer.parseInt(texto);
+			return TipoComando.NUMERO;
+		} catch (NumberFormatException e) {
+			// Quando não for número....
+			if ("AC".equals(texto)) {
+				return TipoComando.ZERA;
+			} else if ("/".equals(texto)) {
+				return TipoComando.DIV;
+			} else if ("*".equals(texto)) {
+				return TipoComando.MULT;
+			} else if ("+".equals(texto)) {
+				return TipoComando.SOMA;
+			} else if ("-".equals(texto)) {
+				return TipoComando.SUB;
+			} else if ("=".equals(texto)) {
+				return TipoComando.IGUAL;
+			} else if (",".equals(texto)) {
+				return TipoComando.VIRGULA;
+			}
+		}
+		return null;
 	}
 }
