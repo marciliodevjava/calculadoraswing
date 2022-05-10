c40f5c90855a15f06deae99287d053ca883d22ba
m-arc <m-arc@192.168.1.8>
2022-05-09 22:33:58 -0300

    Class Teclado

--- src/br/com/cod3r/calc/visao/Teclado.java
+++ src/br/com/cod3r/calc/visao/Teclado.java
@@ -20,33 +20,37 @@ public class Teclado extends JPanel {
 		GridBagConstraints c = new GridBagConstraints();
 
 		setLayout(layout);
+		c.weightx = 1;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
 		// Linha 1
+		c.gridwidth = 3;
 		adicionarBotao("AC", COR_CINZA_ESCUR0, c, 0, 0);
-		adicionarBotao("+/-", COR_CINZA_ESCUR0, c, 1, 0);
-		adicionarBotao("%", COR_CINZA_ESCUR0, c, 2, 0);
+		c.gridwidth = 1;
 		adicionarBotao("/", COR_LARANJA, c, 3, 0);
-		
+
 		// Linha 2
 		adicionarBotao("7", COR_CINZA_CLARO, c, 0, 1);
 		adicionarBotao("8", COR_CINZA_CLARO, c, 1, 1);
 		adicionarBotao("9", COR_CINZA_CLARO, c, 2, 1);
 		adicionarBotao("*", COR_LARANJA, c, 3, 1);
 
-		//Linha 3
+		// Linha 3
 		adicionarBotao("4", COR_CINZA_CLARO, c, 0, 2);
 		adicionarBotao("5", COR_CINZA_CLARO, c, 1, 2);
 		adicionarBotao("6", COR_CINZA_CLARO, c, 2, 2);
 		adicionarBotao("-", COR_LARANJA, c, 3, 2);
-		
-		//Linha 4
+
+		// Linha 4
 		adicionarBotao("1", COR_CINZA_CLARO, c, 0, 3);
 		adicionarBotao("2", COR_CINZA_CLARO, c, 1, 3);
 		adicionarBotao("3", COR_CINZA_CLARO, c, 2, 3);
 		adicionarBotao("+", COR_LARANJA, c, 3, 3);
-		
-		//Linha 5
+
+		// Linha 5
+		c.gridwidth = 2;
 		adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
-		adicionarBotao("0", COR_CINZA_CLARO, c, 1, 4);
+		c.gridwidth = 1;
 		adicionarBotao(",", COR_CINZA_CLARO, c, 2, 4);
 		adicionarBotao("=", COR_LARANJA, c, 3, 4);
 
